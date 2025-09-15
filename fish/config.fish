98211034e08b7bb0a501798da65ea5c14b17e021
Atharv Shirgurkar <atharvshirgurkar@gmail.com>
2025-09-15 11:35:45 +0530

    Initial Commit

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set -U fish_greeting
+fish_config prompt choose arrow
+set -gx XDG_SCREENSHOTS_DIR ~/Pictures/Screenshots
+set -gx QT_QPA_PLATFORMTHEME qt5ct
+set -gx QT_QPA_PLATFORMTHEME qt6ct
